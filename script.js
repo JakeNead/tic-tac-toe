51fee5467ad340648949d21b59e15d57dfd6ece2
Jake Nead <jakenead@gmail.com>
2023-02-21 11:59:36 -0800

    Render gameboardArray onto the DOM

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
+// gameboard module to init the gameboard
+
 const gameboard = (function () {
     let gameboardArray = [
-        'x', 'o', 'x','o', 'x', 'o','x', 'o', 'x'
+        'x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'
     ]
+    // render the gamboard array on the DOM
+    const cells = document.querySelectorAll('.cell')
+    const renderBoard = (cells) => {
+        for (let i=0; i<cells.length; i++) {
+            cells[i].textContent = gameboardArray[i]
+        }
+    }
+    renderBoard(cells)
+    return {
+        renderBoard,
+    }
 })()
 
 // player objects factory?
