6cdfd43b65489dad4ea41eabdbb2cb140903e7b2
Jake Nead <jakenead@gmail.com>
2023-02-21 11:00:46 -0800

    Add .gitignore

---
+++ script.js
@@ -0,0 +1,16 @@
+const gameboard = (function () {
+    let gameboardArray = [
+        ['x', 'o', 'x'],
+        ['o', 'x', 'o']
+        ['x', 'o', 'x']
+    ]
+})()
+
+// player objects factory?
+const playerFactory = (userName) => {
+return {}
+}
+
+const gameplay = (function () {
+})()
+// obj to control flow of game
